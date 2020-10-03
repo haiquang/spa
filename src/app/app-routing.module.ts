2fba07f446ef77bd8be04332d31326fb99eacfad
Quang <quang.nguyen@pivot88.com>
2020-10-03 12:53:17 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '**', redirectTo: 'home'
   }
 ];
 
